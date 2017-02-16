2d541ce5183cf3c72638d80eb0f2dc0bcd08b8da
jackiezjiang <jackiezjiang@gmail.com>
2017-02-16 14:18:02 -0500

    blank activity

---
+++ app/src/main/java/groupb/a818g/friendguard/MainActivity.java
@@ -0,0 +1,118 @@
+package groupb.a818g.friendguard;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final long ONE_MIN = 1000 * 60;
+    private static final long TWO_MIN = ONE_MIN * 2;
+    private static final long FIVE_MIN = ONE_MIN * 5;
+    private static final long MEASURE_TIME = 1000 * 30;
+    private static final long POLLING_FREQ = 1000 * 10;
+    private static final float MIN_ACCURACY = 25.0f;
+    private static final float MIN_LAST_READ_ACCURACY = 500.0f;
+    private static final float MIN_DISTANCE = 10.0f;
+
+    // Views for display location information
+    private TextView mAccuracyView;
+    private TextView mTimeView;
+    private TextView mLatView;
+    private TextView mLngView;
+
+    private int mTextViewColor = Color.GRAY;
+
+    // Current best location estimate
+    private Location mBestReading;
+
+    // Reference to the LocationManager and LocationListener
+    private LocationManager mLocationManager;
+    private LocationListener mLocationListener;
+
+    private final String TAG = "LocationGetLocationActivity";
+
+    private boolean mFirstUpdate = true;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+        mAccuracyView = (TextView) findViewById(R.id.accuracy_view);
+        mTimeView = (TextView) findViewById(R.id.time_view);
+        mLatView = (TextView) findViewById(R.id.lat_view);
+        mLngView = (TextView) findViewById(R.id.lng_view);
+
+        mAccuracyView.setText("accuracy we need");
+        mTimeView.setText("time elapsed");
+        mLatView.setText("Lat position");
+        mLngView.setText("Lng position");
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+
+
+
+
+}
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
